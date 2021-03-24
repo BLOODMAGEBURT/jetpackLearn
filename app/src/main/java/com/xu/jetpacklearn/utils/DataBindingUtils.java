e22444c938b76b5affd74a02f4d093ebb6f059eb
xujianbo <xujianbovip@gmail.com>
2021-03-24 13:34:13 +0800

    budget_expend

--- app/src/main/java/com/xu/jetpacklearn/utils/DataBindingUtils.java
+++ app/src/main/java/com/xu/jetpacklearn/utils/DataBindingUtils.java
@@ -2,6 +2,8 @@ package com.xu.jetpacklearn.utils;
 
 import androidx.databinding.InverseMethod;
 
+import java.text.DecimalFormat;
+
 /**
  * @Author 许剑波
  * 创建时间：2021/3/23  15:57
@@ -20,6 +22,11 @@ public class DataBindingUtils {
 
     public static String convertDoubleToString(double value) {
         return String.valueOf(value);
+    }
+
+    // 将double的钱 以非科学计算的方式转成string
+    public static String formatMoney(double money) {
+        return new DecimalFormat("#,##0.00").format(money);
     }
 
 }
