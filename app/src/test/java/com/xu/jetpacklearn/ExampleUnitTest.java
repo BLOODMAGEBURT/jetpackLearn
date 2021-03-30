f1cfe9bcb39b598047f3a882b5650401b20b35b9
xujianbo <xujianbovip@gmail.com>
2021-03-30 16:47:16 +0800

    test

--- app/src/test/java/com/xu/jetpacklearn/ExampleUnitTest.java
+++ app/src/test/java/com/xu/jetpacklearn/ExampleUnitTest.java
@@ -1,6 +1,13 @@
 package com.xu.jetpacklearn;
 
+import android.util.Log;
+
+import com.github.simonpercic.oklog.shared.data.LogData;
+
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +20,26 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+
+
+
+    @Test
+    // 判断是否是图片 jpg png jpeg
+    public void isPic() {
+        String path = "sys/2c91808278814894017881b03cc20003.jpg";
+        List<String> picFiles = Arrays.asList("jpg", "png", "jpeg", "bmp", "webp", "gif");
+
+        int index = path.lastIndexOf('.');
+
+
+        String end = path.substring(index+1).toLowerCase();
+        System.out.println("end:"+end);
+        if (picFiles.contains(end)) {
+            System.out.println("baohan");
+            return;
+        }
+        System.out.printf("buboahan");
     }
 }
