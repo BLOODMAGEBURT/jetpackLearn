b1ff2e92a319331a2454f52e5bf23715c1fdc580
xujianbo <henghenghalala@163.com>
2021-06-06 16:28:47 +0800

    cordinaotr

---
+++ app/src/main/java/com/xu/jetpacklearn/learn/CordinatorActivity.java
@@ -0,0 +1,16 @@
+package com.xu.jetpacklearn.learn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.xu.jetpacklearn.R;
+
+public class CordinatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cordinator);
+    }
+}
